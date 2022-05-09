1b4e65d4efe551e52ce41bd00a9c4f3255b80b3b
Edward <fantom-exe@tuta.io>
2022-05-09 04:06:01 -0700

    fixed file access methods

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import requests
 
 
 def scraper(us_state) -> str:
+    print('Scraping data ...')
     url = 'https://datausa.io/profile/geo/' + us_state.casefold()
     html_text = requests.get(url).text
 
@@ -14,27 +15,34 @@ def scraper(us_state) -> str:
 
 def txt_srvc():
     def write_to_file(content):
-        print("Writing to file ...")
+        print("Writing to mc-output ...")
         file = open('mc-output.txt', 'w')
-        file.write(str(content))
+        file.write(content)
         file.close()
+        del_contents()
 
     def read_from_file():
-        print("Reading from file ...")
+        print("Reading from mc-input ...")
         file = open('mc-input.txt', 'r')
         return file.readline()
 
+    def del_contents():
+        print("Emptying input file ...")
+        file = open('mc-input.txt', 'w')
+        file.write('')
+        file.close()
+
     while True:
-        time.sleep(2)
+        time.sleep(1)
         line = read_from_file()
-        if line not None:
+        if line is not None:
+            print('US State found!')
             income = scraper(line)
-            write_to_file()
+            write_to_file(income)
 
 
 def main():
     txt_srvc()
-    print()
 
 
 main()
